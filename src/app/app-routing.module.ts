87f15f9c5664333f6a3a05e1bd580bef72b5b7b4
Xiguana <jak.nic98@gmail.com>
2020-04-20 12:53:29 +0200

    Created basic components and added routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,38 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {MainLayoutComponent} from './MainPage/main-layout/main-layout.component'
+import {BookListComponent} from './Books/book-list/book-list.component'
+import {BookAddComponent} from './Books/book-add/book-add.component'
+import {BookDetailsComponent} from './Books/book-details/book-details.component'
+import {BookEditComponent} from './Books/book-edit/book-edit.component'
+import {LoginComponent} from './Login/login/login.component'
+import {RegisterComponent} from './Login/register/register.component'
+import {AuthorListComponent} from './Authors/author-list/author-list.component'
+import {AuthorAddComponent} from './Authors/author-add/author-add.component'
+import {AuthorEditComponent} from './Authors/author-edit/author-edit.component'
+import {UserListComponent} from './Users/user-list/user-list.component'
+import {UserAddComponent} from './Users/user-add/user-add.component'
+import {UserEditComponent} from './Users/user-edit/user-edit.component'
+import {AdminBookListComponent} from './Books/admin-book-list/admin-book-list.component'
+const routes: Routes = [
 
+  {path:'', component:MainLayoutComponent, pathMatch:'full'},
+  {path:'admin/users',component:UserListComponent},
+  {path:'admin/users/:id',component:UserEditComponent},
+  {path:'admin/users/new',component:UserAddComponent},
+  {path:'books',component:BookListComponent},
+  {path:'books/:id',component:BookDetailsComponent},
+  {path:'admin/books/new',component:BookAddComponent},
+  {path:'admin/books',component:AdminBookListComponent},
+  {path:'admin/books/:id',component:BookEditComponent},
+  {path:'login',component:LoginComponent},
+  {path:'register',component:RegisterComponent},
+  {path:'admin/authors',component:AuthorListComponent},
+  {path:'admin/authors/:id',component:AuthorEditComponent},
+  {path:'admin/authors/new',component:AuthorAddComponent},
 
-const routes: Routes = [];
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,18 @@ import { AppComponent } from './app.component';
 import { BooksComponent } from './books/books.component';
 import { BookEditComponent } from './books/book-edit/book-edit.component';
 import { BookAddComponent } from './books/book-add/book-add.component';
+import { BookListComponent } from './Books/book-list/book-list.component';
+import { UserListComponent } from './Users/user-list/user-list.component';
+import { UserAddComponent } from './Users/user-add/user-add.component';
+import { UserEditComponent } from './Users/user-edit/user-edit.component';
+import { AuthorListComponent } from './Authors/author-list/author-list.component';
+import { AuthorEditComponent } from './Authors/author-edit/author-edit.component';
+import { AuthorAddComponent } from './Authors/author-add/author-add.component';
+import { BookDetailsComponent } from './Books/book-details/book-details.component';
+import { LoginComponent } from './Login/login/login.component';
+import { RegisterComponent } from './Login/register/register.component';
+import { MainLayoutComponent } from './MainPage/main-layout/main-layout.component';
+import { AdminBookListComponent } from './Books/admin-book-list/admin-book-list.component';
 
 @NgModule({
   declarations: [
@@ -15,7 +27,19 @@ import { BookAddComponent } from './books/book-add/book-add.component';
     FormsModule,
     HttpClientModule,
     BookEditComponent,
-    BookAddComponent
+    BookAddComponent,
+    BookListComponent,
+    UserListComponent,
+    UserAddComponent,
+    UserEditComponent,
+    AuthorListComponent,
+    AuthorEditComponent,
+    AuthorAddComponent,
+    BookDetailsComponent,
+    LoginComponent,
+    RegisterComponent,
+    MainLayoutComponent,
+    AdminBookListComponent
   ],
   imports: [
     BrowserModule,
